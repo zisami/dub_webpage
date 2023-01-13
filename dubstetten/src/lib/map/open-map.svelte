4a140f0062c6dfb5e49408efb1c0d99b5870ed30
gobi <gabrielengelhart@gmx.at>
2023-01-13 16:47:57 +0100

    maps fix

--- dubstetten/src/lib/map/open-map.svelte
+++ dubstetten/src/lib/map/open-map.svelte
@@ -1,17 +1,13 @@
-<iframe
-	scrolling="no"
-	marginheight="0"
-	marginwidth="0"
-	src="https://www.openstreetmap.org/export/embed.html?bbox=14.989806711673738%2C48.319853541330176%2C14.993065595626833%2C48.3213838410101&layer=mapnik&marker=48.32061869690858%2C14.991437494754791"
-	style="border: 1px solid black"
-	width="100%"
-	height="400"
-	frameborder="0"
-	title="Dubstetten-Map"
-/>
-<br />
+<iframe src="https://www.openstreetmap.org/export/embed.html?bbox=14.990222454071047%2C48.320101460128775%2C14.992684721946718%2C48.32113771199181&amp;layer=mapnik&amp;marker=48.32061958869159%2C14.99145358800888" 
+	width="100%" 
+	height="350" 
+	frameborder="0" 
+	scrolling="yes" 
+	marginheight="0" 
+	marginwidth="0" 
+	style="border: 1px solid black">
+</iframe>
+<br/>
 <small>
-	<a href="https://www.openstreetmap.org/?mlat=48.32062&amp;mlon=14.99144#map=19/48.32062/14.99144"
-		>Größere Karte anzeigen</a
-	>
+	<a href="https://www.openstreetmap.org/?mlat=48.32062&amp;mlon=14.99145#map=19/48.32062/14.99145">Größere Karte anzeigen</a>
 </small>
