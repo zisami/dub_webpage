d046f206c0b4292215829388b293fe7c7823c21f
gobi <gabrielengelhart@gmx.at>
2023-01-20 00:50:56 +0100

    remove </a>

--- src/lib/programm/programm-list.svelte
+++ src/lib/programm/programm-list.svelte
@@ -1,12 +1,12 @@
 <script>
-    import { programm } from '$lib/programm/programm';
-    import ProgrammCard from '$lib/programm/programm-card.svelte';
+    import { programm } from "$lib/programm/programm";
+    import ProgrammCard from "$lib/programm/programm-card.svelte";
 </script>
 
 <div id="programm-list">
     {#each programm as programItem}
         {#if programItem.slug}
-            <a href={programItem.slug}></a>
+            <a href={programItem.slug}>
                 <ProgrammCard {programItem} />
             </a>
         {:else}
