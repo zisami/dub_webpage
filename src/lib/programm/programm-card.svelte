33bd91797b26dcef90c38fee397224f8cbbf6739
zisami <sascha.zika@gmx.at>
2023-01-19 23:35:28 +0100

    lineup

--- src/lib/programm/programm-card.svelte
+++ src/lib/programm/programm-card.svelte
@@ -1,53 +1,49 @@
 <script lang="ts">
-  import type { ProgrammItem } from "$lib/programm/programm";
-  export let programItem: ProgrammItem;
+    import type { ProgrammItem } from '$lib/programm/programm';
+    export let programItem: ProgrammItem;
 </script>
 
 <div class="programm-item">
-  <div class="thumb">
-    <img src={programItem.thumbnail} alt={programItem.title} />
-  </div>
-  <div class="content">
-    <h2>{programItem.title}</h2>
-    <p>{programItem.description}</p>
+    <div class="thumb">
+        <img src={programItem.thumbnail} alt={programItem.title} />
+    </div>
+    <div class="content">
+        <h2>{programItem.title}</h2>
+        <p>{programItem.description}</p>
 
-    <div class="start">{programItem.startsAt}</div>
-    <!--würd ich gerne in eine box rechts unten packen -->
-  </div>
+        <div class="start">{programItem.startsAt}</div>
+        <!--würd ich gerne in eine box rechts unten packen -->
+    </div>
 </div>
 
 <style>
-  .programm-item {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #eee;
-    margin: 1rem 1rem;
-    border-radius: 0rem;
-    overflow: hidden;
-    box-shadow: rgb(0, 0, 0, 0.1) 0.5rem 0 1rem;
-    max-width: 80vw;
-  }
-
-  .content {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem 2rem;
-  }
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-  @media screen and (min-width: 600px) {
     .programm-item {
-      max-width: 30vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #eee;
+        margin: 1rem 1rem;
+        border-radius: 0rem;
+        overflow: hidden;
+        box-shadow: rgb(0, 0, 0, 0.1) 0.5rem 0 1rem;
+        max-width: 100%;
     }
-  }
-  /* place timtable time
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem 2rem;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    /* place timtable time
   
         .start {
         display: flex;
--- src/lib/programm/programm-list.svelte
+++ src/lib/programm/programm-list.svelte
@@ -1,25 +1,40 @@
 <script>
-  import { programm } from "$lib/programm/programm";
-  import ProgrammCard from "$lib/programm/programm-card.svelte";
+    import { programm } from '$lib/programm/programm';
+    import ProgrammCard from '$lib/programm/programm-card.svelte';
 </script>
 
 <div id="programm-list">
-  {#each programm as programItem}
-    {#if programItem.slug}
-      <a href={programItem.slug}>
-        <ProgrammCard {programItem} />
-      </a>
-    {:else}
-      <ProgrammCard {programItem} />
-    {/if}
-  {/each}
+    {#each programm as programItem}
+        {#if programItem.slug}
+            <a href={programItem.slug}></a>
+                <ProgrammCard {programItem} />
+            </a>
+        {:else}
+            <div>
+                <ProgrammCard {programItem} />
+            </div>
+        {/if}
+    {/each}
 </div>
 
 <style>
-  #programm-list {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-  }
+    #programm-list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: stretch;
+    }
+    #programm-list > * {
+        width: 100%;
+    }
+    @media (min-width: 768px) {
+        #programm-list > * {
+            width: 50%;
+        }
+    }
+    @media (min-width: 1024px) {
+        #programm-list > * {
+            width: 33.333%;
+        }
+    }
 </style>
